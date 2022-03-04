24797a84d5fd4ceefd77b40e66a2e65909f09acb
Arseni Skobelev <arseni.skobelev0@gmail.com>
2022-03-04 05:49:42 +0100

    App done. Readme done. 04.03.22

---
+++ README.md
@@ -0,0 +1,23 @@
+
+# React color palette generator app
+
+This is a small project made to practice React skills and use The Color API. This was also created to practice array functions (Array.map() etc.)
+
+&nbsp;  
+
+
+
+
+
+
+## Technologies used
+
+- React JS 
+- Tailwind CSS
+- The Color API
+
+&nbsp;
+
+## Screenshot
+
+![Failed to load screenshot](\readmeScreens\main.png "Screenshot of the interface")
--- src/components/ColorPalette.js
+++ src/components/ColorPalette.js
@@ -1,65 +1,112 @@
 import React, { useEffect, useState } from 'react'
 import Button from './Button'
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
+
+var randomHexColor = require('random-hex-color')
 
 export default function ColorPalette() {
-    const [generateClicked, setGenerateClicked] = useState(false)
-    const [apiAnswer, setApiAnswer] = useState({})
-    let arrayOfColors = []
-    let [data, setData] = useState()
-
-
-    const CreateCubes = ({colorArray}) => (
-        <div>
-            {arrayOfColors.map(function (colorArray, i) {
-                console.log("no")
-                return <div style={{background: arrayOfColors[i]}} className={`w-[100px] h-[100px] m-2`} key={i}></div>
-            })}
-        </div>
-    );
+    // states
+    const [mode, setMode] = useState('')
+    
+    const [arrayOfColors, setColors] = useState([])
+    
+    // variables
+    let fetchMode = '';
+    
+    let fetchLink = '';
+    
+    let randomHex = randomHexColor();
+    
+    let newMode;
+    
+    let colorsArr = ["#000000", "#000000", "#000000", "#000000", "#000000", "#000000"]
     
     
-    useEffect(() => {
-        fetch("https://www.thecolorapi.com/scheme?hex=24B1E0&mode=triad&count=6")
+    // dropdown options
+    const options = [
+        'Monochrome', 
+        'Monochrome dark', 
+        'Monochrome light', 
+        'Analogic',
+        'Complement',
+        'Analogic complement',
+        'Triad',
+        'Quad'
+    ];
+    
+    const defaultOption = options[0];
+    
+    // functions
+    const handleChange = (e) => {
+        setMode(e.value);
+    }
+
+    // button onClick; get mode from dropdown, get full fetch link and fetch api
+    const changeState = () => {
+        newMode = mode.toLowerCase().split(' ').join('-');
+        fetchMode = newMode;
+        fetchLink = `https://www.thecolorapi.com/scheme?hex=${randomHex.substring(1)}&mode=${fetchMode}&count=6`
+        getColors()
+    }
+    
+    // main fetch function
+    const getColors = () => {
+        // clear array each time the function is called
+        colorsArr = [];
+
+        // fetch api (www.thecolorapi.com) with the link that was created before
+        fetch(fetchLink)
         .then(res => res.json())
         .then(
             (result) => {
-                setApiAnswer(result.colors)
-                // console.log(result.colors)
-                for (let index = 0; index < apiAnswer.length; index++) {
-                    // console.log(apiAnswer[index].hex.value)
-                    arrayOfColors.push(apiAnswer[index].hex.value);
+                // loop through the colors returned by the api
+                for (let index = 0; index < result.colors.length; index++) {
+                    // add each color to the array
+                    colorsArr.push(result.colors[index].hex.value)
                 }
-                setData(arrayOfColors)
+
+                // set the state to the array value
+                setColors(colorsArr);
             },
             (error) => {
+                // console log if there are any errors
                 console.log(error)
             }
-            )
-    
-      return () => {
-        
-      }
-    }, [generateClicked])
+        )
+
+    }
+
     
     return (
-    <div className='pb-8 pt-8 h-full flex justify-center items-center flex-col'>
+    <div className='h-full flex justify-center items-center flex-col'>
 
         {/* Header */}
-        <h1 className='text-2xl text-center'>
-            Color Palette Generator
-        </h1>
+        <div>
+            <h1 className='text-2xl text-center font-medium'>
+                Color Palette Generator
+            </h1>
+        </div>
+
+        {/* Mode choice dropdown */}
+        <div className='pt-4'>
+            <h2 className='text-center mb-1'><i>Choose palette mode</i></h2>
+            <Dropdown options={options} onChange={handleChange} value={defaultOption} placeholder="Select an option" />
+        </div>
 
         {/* Color display */}
-        <div className='flex justify-center items-center flex-row flex-wrap py-20'>
-            <div className='bg-gray-500 w-[100px] h-[100px] m-2'></div>
-            {data.map(function (data, i) {
-                return <div style={{background: data[i]}} className={`w-[100px] h-[100px] m-2`} key={i}></div>
-            })}
+        <div className='flex justify-center items-center flex-row flex-wrap py-5'>
+            { 
+                colorsArr.map(function (data, i) {
+                    return <div style={{background: arrayOfColors[i]}} className={`w-[100px] h-[100px] m-2`} key={i}></div>;
+                })
+            }
         </div>
+
 
         {/* Button */}
         <div className='flex justify-center items-center'>
-            <Button onClick={() => setGenerateClicked(!generateClicked)} text="Generate" />
+            <Button onClick={changeState} text="Generate" />
         </div>
     </div>
   )
